93b89f661e1e0bedccc93133a6c6add743d0a2d0
The-Vast <sanusisolvent@gmail.com>
2022-06-09 08:51:31 +0000

    create funtion that converts a Roman numeral to an integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """that converts a Roman numeral to an integer."""
+    if isinstance(roman_string, str) or roman_string != None:
+        major = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
+        main = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        integer = main[roman_string[0]]
+        if len(roman_string) == 1:
+            return integer
+        for i in range(1, len(roman_string)):
+            if main[roman_string[i]] > main[roman_string[i-1]]:
+                integer = main[roman_string[i]] - integer
+            else:
+                integer += main[roman_string[i]]
+        return integer
+    return 0
