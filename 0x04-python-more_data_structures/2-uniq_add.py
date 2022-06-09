230474f9b9fd80eb977170a85f6bf013bec84b39
The-Vast <sanusisolvent@gmail.com>
2022-06-09 05:38:30 +0000

    fix bug

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-   """adds all unique integers in a list"""
+    """adds all unique integers in a list"""
     conv_set = set(my_list)
     return sum(list(conv_set))
