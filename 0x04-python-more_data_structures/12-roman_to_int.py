23b142d4593d4393a98aaea143b010fb9ef97772
The-Vast <sanusisolvent@gmail.com>
2022-06-10 00:13:57 +0000

    fix pycodestyle requirement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """that converts a Roman numeral to an integer"""
     if isinstance(roman_string, str) and roman_string is not None:
         main = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         integer = main[roman_string[-1]]
@@ -12,4 +11,4 @@ def roman_to_int(roman_string):
             else:
                 integer += main[roman_string[i-1]]
         return integer
-    return 0
+    return (0)
