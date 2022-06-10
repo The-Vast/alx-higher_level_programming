88d56a81a52ac9ff432e8d8b59de7d6be2c16eaa
The-Vast <sanusisolvent@gmail.com>
2022-06-10 00:23:16 +0000

    fix pycodestyle requirement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,9 @@ def roman_to_int(roman_string):
         if len(roman_string) == 1:
             return integer
         for i in range(len(roman_string) - 1, 0, -1):
-            if main[roman_string[i]] > main[roman_string[i-1]]:
-                integer -= main[roman_string[i-1]]
+            if main[roman_string[i]] > main[roman_string[i - 1]]:
+                integer -= main[roman_string[i - 1]]
             else:
-                integer += main[roman_string[i-1]]
+                integer += main[roman_string[i - 1]]
         return integer
     return (0)
