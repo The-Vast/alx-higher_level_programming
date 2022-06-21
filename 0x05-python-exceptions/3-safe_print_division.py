1cda13bbeb930ef1f0a61422a5ed996584cacdaa
SanusiWasiu <wasiu@seamlesshr.com>
2022-06-21 01:49:18 +0100

    add function that divides element by element 2 lists

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,9 @@ def safe_print_division(a, b):
     try:
         division = a / b
     except (TypeError, ZeroDivisionError):
-        div = None
+        division = None
     finally:
-        print("Inside result: {}".format(div))
-    return div
-    
+        print("Inside result: {}".format(division))
+    return division
 
 
