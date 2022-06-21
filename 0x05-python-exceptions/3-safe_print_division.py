e10a3827cd807a0ef2062f9951a0f9ef7060e668
SanusiWasiu <wasiu@seamlesshr.com>
2022-06-21 01:41:39 +0100

    add function that divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        return(a / b)
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
+    
 
 
