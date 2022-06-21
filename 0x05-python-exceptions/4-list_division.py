8017aa8b74b6f02b956743ed6683eea4e694bc24
SanusiWasiu <wasiu@seamlesshr.com>
2022-06-21 01:57:40 +0100

    add function that raises a name exception with a message.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             division = 0
         except ZeroDivisionError:
             print("division by 0")
-            divison = 0
+            division = 0
         except IndexError:
             print("out of range")
             division = 0
