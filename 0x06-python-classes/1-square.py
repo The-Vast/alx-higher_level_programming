8ff01e53b8460426e7cf64f6bb43fa5c73de42b7
The-Vast <sanusisolvent@gmail.com>
2022-06-22 21:22:42 +0000

    add square class instantiated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square that defines a square by:.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    You are not allowed to import any module.
+"""
+class Square:
+    """This is the Square."""
+
+    def __init__(self, size):
+        """Initialize the square
+        Args:                                                                                                                                                size (int): The size of a square.                                                                                                      
+        """
+        self.size = __size
