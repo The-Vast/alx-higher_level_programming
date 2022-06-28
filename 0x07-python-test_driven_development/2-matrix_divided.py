89836ad91c2e1dd02290050f4a050c24c732282b
SanusiWasiu <wasiu@seamlesshr.com>
2022-06-28 19:52:08 +0100

    fix bugs

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,8 +21,8 @@ def matrix_divided(matrix, div):
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
-            not all(isinstance(col, int) or isinstance(col, float)
-                for col in [num for row in matrix for num in row]):
+            not all((isinstance(col, int) or isinstance(col, float))
+                for col in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
 
