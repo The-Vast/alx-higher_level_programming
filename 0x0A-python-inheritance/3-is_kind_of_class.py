d315f0f8db146538d3f2338054c742bf2dedd752
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-06 01:16:33 +0100

    add function that verifies if an object is an instance or inherited instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """verifies if an object is an instance or inherited instance of a class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an instance or inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
