e2563202da01de463172aad1502c44812028207f
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-06 23:33:05 +0100

    fix class name error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,9 +3,8 @@
 """Only sub class of"""
 
 
-def is_kind_of_class(obj, a_class):
-    """verifies if an object is an instance of a class
-    that inherited (directly or indirectly) from the specified class.
+def inherits_from(obj, a_class):
+    """verifies if an object is an inherited instance of a class.
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
