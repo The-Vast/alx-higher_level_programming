1fb65fa6a13b055f95a84755dd72909c27ded659
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-06 01:47:05 +0100

    add rectangle area and str function

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Rectangle"""
+"""Full rectangle"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -17,3 +17,13 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
