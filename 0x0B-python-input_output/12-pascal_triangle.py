1366c317b7503673618a95d79e81e5ca6938f797
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-11 16:02:22 +0100

    add Pascal Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    
+    triangles = [[1]]
+    while len(triangles) != n:
+        triangle = triangles[-1]
+        tmp = [1]
+        for i in range(len(triangle) - 1):
+            tmp.append(triangle[i] + triangle[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
