74eb4537bb692c4b2c6db927a655b37f9a7b482a
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-12 00:59:10 +0100

    fix base initialization bug

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+    """defines base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """instantiation of base object"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
