8daf78fdf6e61084b2f030465e03241d99d8767f
SanusiWasiu <wasiu@seamlesshr.com>
2022-07-13 23:06:48 +0100

    add rectangle class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,15 @@
 class Base:
     """defines base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        """instantiation of base object"""
-        if id != None:
+        """
+        instantiation of base object
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
